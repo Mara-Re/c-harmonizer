1034334de1c676c1a6d3e74c1655f7f17e530a7b
Mara <mara.js@t-online.de>
2019-12-17 17:01:38 +0100

    basic setup codon usage table

--- src/results.js
+++ src/results.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useSelector} from 'react-redux'; 
+
 import Graph from './graph';
 import HarmonizedGene from './harmonized-gene';
+import TableCodonUsage from './table-codon-usage';
 
-export default function Results() {
-    
+export default function Results() {  
+
+    const results = useSelector(state => {
+        return state.results 
+    });
+
+    useEffect(() => {
+        try {
+            results && sessionStorage.setItem('results', JSON.stringify(results));
+          } catch (e) {
+            console.log('Error sessionStorage results: ', e);
+          }
+    }, [results]);
+  
 
     return (
         <>
             <HarmonizedGene />            
             <Graph />
+            <TableCodonUsage />
         </>
     );
 };
